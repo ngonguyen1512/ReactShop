078e6745f8236fdd52bd93a394eab1e8743b6ca1
Nguyen <94852638+ngonguyen1512@users.noreply.github.com>
2023-11-23 10:25:00 +0700

    23/11 update

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+require('dotenv').config()
+import cors from 'cors'
+import initRoutes from './src/routes'
+// import cons from './src/config/connectDatabase'
+
+const app = express();
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    method: ["POST", "PUT", "DELETE", "GET"]
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+initRoutes(app);
+// cons(app);
+
+const port = process.env.PORT || 8888;
+const listener = app.listen(port, () => {
+    console.log(`Server listening on port: http://localhost:${listener.address().port}`);
+});
---
+++ server/src/routes/auth.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as authController from '../controllers';
+
+const router = express.Router();
+
+// router.post('/login', authController.login)
+// router.post('/register', authController.register)
+// router.post('/forgot', authController.forgotPassword)
+
+export default router;
